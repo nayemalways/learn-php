7f87e28fb5dc1a40926e5652a9943f689d1fc091
Nayem Ahmed <nishanahmed13913@gmail.com>
2026-08-25 01:46:40 +0600

    learend string

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # Learning PHP
 
 ## What I learned Today (Aug 23)
-    1. PHP basic syntax
-    2. PHP Comment
-    3. PHP Variables
-    4. PHP Variable Interpolation
-    5. PHP Variables type check by **var_dump()**
+1. PHP basic syntax
+2. PHP Comment
+3. PHP Variables
+4. PHP Variable Interpolation
+5. PHP Variables type check by **var_dump()**
+6. Variables Scope, Global, Local and Static
+7. global and super $GLOBAL.
+8. String Data Type
+9. String Function - strlen(), str_contains(), strrev(), str_word_count()
--- index.php
+++ index.php
@@ -27,9 +27,132 @@
 // var_dump($age);
 
 # PHP Variables Scope
+# Global Scope and Function Scope
 function myTest() {
     global $name; // To access global variable, assign the var by global flag. Otherwise the global var can't be accessible from function scope.
     echo "My name is $name";
+
+    $weather = 'Today is a Sunny Day'; // Local Scope: A local scope variables belongs inside a function. This variable not accessible from global scope.
+    echo $weather;
+    
 }
 
-myTest();
+// myTest();
+
+
+# Variables Static Scope
+function testStatic() {
+    static $x = 0;
+    echo $x;
+    $x++;
+};
+
+// testStatic();
+// testStatic();
+// testStatic();
+
+# Super Global $GLOBAL
+$x = 10;
+$y = 5;
+function testGlobalOne() {
+    $GLOBALS['y'] = $GLOBALS['x'] + $GLOBALS['y'];
+};
+
+// testGlobalOne();
+// echo $y;
+
+function testGlobalTwo() {
+    global $x, $y;
+    $x = $x + $y;
+};
+
+// testGlobalTwo();
+// echo $x; 
+
+// print 'Hello PHP world, Today I joined with the PHP community';
+// echo 'Hello PHP world, Today I joined with the PHP community';
+
+# PHP Data Types
+/*
+    1. string
+    2. int (Numbers)
+    3. float (Decimal numbers)
+    4. bool (Boolean)
+    5. array (Array)
+    6. object (Object)
+    7. null (Null)
+    8. resource (Reference external resources)
+*/
+
+# string
+$a = 'Apple';
+# int
+$b = 34;
+# float
+$c = 45.54;
+# bool
+$d = true;
+# array
+$e = ['Hello', 34,];
+# object
+class User {
+    public $name;
+    public $age;
+}
+
+$user = new User();
+$user-> name = 'Nayem';
+$user-> age = 23;
+
+// echo $user->name;
+
+
+// PHP IF-ELSE CONDITION
+// if ($age > 18) {
+//     echo "The man is adult";
+// }else {
+//     echo "He is a child";
+// }
+
+// PHP Object Data Type
+class Car {
+    public $color;
+    public $model;
+
+    public function __construct($color, $model) {
+        $this->color = $color;
+        $this->model = $model;
+    }
+
+    function carDetails() {
+        return "My car is a $this->color and model is $this->model";
+    }
+}
+
+$myCar = new Car("Blue", "B305");
+// echo $myCar->carDetails();
+// echo $myCar->color;
+
+
+# PHP String Functions
+$school = 'A Rob School and College';
+
+# Check String length strlen().
+// echo strlen($school); 
+// echo strlen('My name is nayem ahmed');
+// echo strlen('448444');
+
+# String word count
+//  echo str_word_count($school);
+//  echo str_word_count('Our country name is Bangladesh. Bangladesh is play cricket. Recently Bangladesh won a historic test match in Australia with Australia. That was mind blowing performance from Bangladesh Team. And for a hard pitch and moving pitch of second test. Also lost the toss, we lost the game.');
+
+# String Reverse
+// echo strrev($school);
+// echo strrev('Nayem Ahmed');
+
+# Search specific substring in a string
+// var_dump(str_contains($school, 'and')); // True
+// var_dump(str_contains($school, 'andd')); // False
+
+# Search specific text within a  string.
+echo strpos($school, 'and');
